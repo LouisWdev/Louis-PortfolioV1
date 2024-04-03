c4d6db3d856e43842acebc16123fced7341869da
LouisWackenier <Louis.wackenier@gmail.com>
2024-04-03 22:10:03 +0200

    Continued styling it but need to add the arwes library

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,14 +1,34 @@
 import { Animator } from '@arwes/react-animator';
 import { Text } from '@arwes/react-text';
+import styled from 'styled-components';
 
 const Home = () => {
   return (
     <Animator>
-      <Text as='p' style={{ color: '#ddd' }}>
-         Welcome to my Portfolio Home page
-      </Text>
+      <CenterContainer>
+        <Text as='h1' style={{ color: '#5728d6', textAlign: 'center'}}>
+          Louis 
+        </Text>
+        <Text as='h1' style={{ color: '#5728d6', textAlign: 'center'}}>
+          Welcome to my Portfolio
+        </Text>
+        <ButtonsContainer>
+          <Text as='p' style={{ color: '#5728d6', display: 'inline-block', marginRight: '10px' }}>Portfolio</Text>
+          <Text as='p' style={{ color: '#5728d6', display: 'inline-block', marginRight: '10px' }}>Contact</Text>
+          <Text as='p' style={{ color: '#5728d6', display: 'inline-block', marginRight: '10px' }}>About Me</Text>
+        </ButtonsContainer>
+      </CenterContainer>
     </Animator>
   )
 }
 
-export default Home
+export default Home;
+
+const CenterContainer = styled.div`
+  margin-top: 40vh;
+`;
+
+const ButtonsContainer = styled.div`
+  text-align: center;
+  cursor: pointer;
+`;
