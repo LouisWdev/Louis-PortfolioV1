4ff154df0986ce0ab32a9f5b4e864551af4e8e9c
Louis <louis.wackenier@gmail.com>
2024-04-09 18:31:12 +0200

    Navbar changes

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,7 +6,10 @@ const Navbar = () => {
   return (
     <Animator>
       <NavbarContainer>
-       <Text as='p' style={{ color: 'white', display: 'inline-block', marginRight: '10px' }}>Home</Text>
+       <Text as='p' style={{ color: 'white', display: 'inline-block',marginLeft: '10px' }}>Home</Text>
+       <Text as='p' style={{ color: 'white', display: 'inline-block', marginLeft: '10px' }}>Contact</Text>
+       <Text as='p' style={{ color: 'white', display: 'inline-block', marginLeft: '10px' }}>Portfolio</Text>
+       <Text as='p' style={{ color: 'white', display: 'inline-block', marginLeft: '10px'}}>About</Text>
       </NavbarContainer>
     </Animator>
   )
@@ -16,4 +19,6 @@ export default Navbar
 
 const NavbarContainer = styled.div`
      text-align: right;
+     margin-right: 10px;
+     cursor: pointer;
 `;
