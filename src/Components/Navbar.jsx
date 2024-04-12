d00cfbb169c046d29649f037bc2caba024d8339e
LouisWackenier <Louis.wackenier@gmail.com>
2024-04-12 19:55:33 +0200

    Working on the react active animation

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,10 +1,18 @@
+import React, { useState, useEffect } from 'react';
 import { Animator } from '@arwes/react-animator';
 import { Text } from '@arwes/react-text';
 import styled from 'styled-components';
 
 const Navbar = () => {
+  const [active, setActive] = useState(true);
+
+  useEffect(() => {
+    const tid = setInterval(() => setActive(active => !active), 2000);
+    return () => clearInterval(tid);
+  }, []);
+
   return (
-    <Animator>
+    <Animator active={active} duration={{ enter: 1.5, exit: 1.5 }}>
       <NavbarContainer>
        <Text as='p' style={{ color: 'white', display: 'inline-block', marginLeft: '15px' }}>Home</Text>
        <Text as='p' style={{ color: 'white', display: 'inline-block', marginLeft: '15px' }}>Contact</Text>
