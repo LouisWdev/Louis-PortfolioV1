828777797d3f500ae16293c371a7dc8e2bed45f2
LouisWackenier <Louis.wackenier@gmail.com>
2024-04-15 08:17:50 +0200

    Changed some settings in the css and worked on the navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -66,9 +66,8 @@ const NavbarContainer = styled.div`
 
 const TextContainer = styled.div`
     width: 75px;
-    font-size: 2vh;
-    padding-left: 20px;
-    padding-right: 40px;
+    font-size: 1rem;
+    padding: 10px 20px 5px 10px;
     display: inline-block;
     transition: all 0.5s ease-in;
      
