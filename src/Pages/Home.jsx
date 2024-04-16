f06f86066de4674b714e87b8a63f5bda867de073
LouisWackenier <Louis.wackenier@gmail.com>
2024-04-16 06:59:32 +0200

    Styling changes to the home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,19 +6,19 @@ const Home = () => {
   return (
     <Animator>
       <CenterContainer>
-        <Text as='h1' style={{ color: 'white', textAlign: 'center'}}>
+        <Text as='h1' style={{ color: 'white', textAlign: 'center', marginBottom: '5px'}}>
           Louis W.
         </Text>
-        <Text as='h2' style={{ color: 'white', textAlign: 'center'}}>
+        <Text as='h2' style={{ color: 'white', textAlign: 'center', marginBottom: '5px'}}>
           Welcome to my Portfolio
         </Text>
         <Text as='h3' style={{ color: 'white', textAlign: 'center'}}>
           Feel free to explore my website
         </Text>
         <ButtonsContainer>
-          <Text as='p' style={{ color: 'white', display: 'inline-block', marginRight: '15px' }}>Portfolio</Text>
-          <Text as='p' style={{ color: 'white', display: 'inline-block', marginRight: '15px' }}>Contact</Text>
-          <Text as='p' style={{ color: 'white', display: 'inline-block', marginRight: '15px' }}>About Me</Text>
+          <ButtonContainer><Text as='p' style={{ color: 'white', display: 'inline-block', marginRight: '15px' }}>Portfolio</Text></ButtonContainer>
+          <ButtonContainer><Text as='p' style={{ color: 'white', display: 'inline-block', marginRight: '15px' }}>Contact</Text></ButtonContainer>
+          <ButtonContainer><Text as='p' style={{ color: 'white', display: 'inline-block', marginRight: '15px' }}>About</Text></ButtonContainer>
         </ButtonsContainer>
       </CenterContainer>
     </Animator>
@@ -32,6 +32,20 @@ const CenterContainer = styled.div`
 `;
 
 const ButtonsContainer = styled.div`
+  margin-top: 5px;
   text-align: center;
   cursor: pointer;
+`;
+
+const ButtonContainer = styled.div`
+    width: 75px;
+    font-size: 1rem;
+    padding: 10px 2px 5px 15px;
+    display: inline-block;
+    transition: border 0.5s ease-in;
+     
+    &:hover {
+      border: 2px solid white;
+      border-radius: 5px;
+    }
 `;
