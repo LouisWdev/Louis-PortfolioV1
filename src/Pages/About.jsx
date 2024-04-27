1b53eb95fce32accf7a7de95b3c066b7caee927e
LouisWackenier <Louis.wackenier@gmail.com>
2024-04-27 08:21:29 +0200

    Worked on the about me page

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,12 +1,29 @@
 import Navbar from "../Components/Navbar";
+
+import { Text } from "@arwes/react";
+import styled from "styled-components";
+
+
 const About = () => {
   return (
-   <>
-    <Navbar />
-    <div>About</div>
-   </>
-    
+  <>
+  <Navbar />
+    <MainContainer>
+      <TextContainer>
+       <Text>Welcome to my about page</Text>
+      </TextContainer>
+    </MainContainer> 
+  </>
   )
 }
 
 export default About
+
+//STYLED SECTION 
+const MainContainer = styled.div`
+    background-color: red;
+    text-align: center;
+    margin-top: 5vh;
+`;
+
+const TextContainer = styled.div``;
