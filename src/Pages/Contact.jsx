f3a25d59b02f590e520d5ff78d2aaacac321d83e
LouisWackenier <Louis.wackenier@gmail.com>
2024-05-07 06:50:57 +0200

    Positioning

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,16 +1,24 @@
-import Navbar from "../Components/Navbar"
+import Navbar from "../Components/Navbar";
+import styled from "styled-components";
 
 const Contact = () => {
   return (
     <>
       <Navbar/>
-      <form>
-        <input type="text" name="email" /><br/>
-        <input type="text" name="description" /><br/>
-        <button>Send</button>
-      </form>
+      <FormContainer>
+       <form>
+         <input type="text" name="email" /><br/>
+         <input type="text" name="description" /><br/>
+         <button>Send</button>
+       </form>
+      </FormContainer>
     </>
   )
 }
 
 export default Contact
+
+const FormContainer = styled.div`
+      margin-top: 35vh;
+      margin-left: 45vw;
+`;
--- src/Pages/Portfolio.jsx
+++ src/Pages/Portfolio.jsx
@@ -1,13 +1,21 @@
-import Navbar from "../Components/Navbar"
+import Navbar from "../Components/Navbar";
+import styled from "styled-components";
 
 const Portfolio = () => {
   return (
     <>
     <Navbar />
-    <div>The Portfolio section will be here</div>
+    <PortfolioContainer>
+     <div>The Portfolio section will be here</div>
+    </PortfolioContainer>
     </>
     
   )
 }
 
 export default Portfolio
+
+const PortfolioContainer = styled.div`
+      margin-top: 35vh;
+      margin-left: 45vw;
+`;
