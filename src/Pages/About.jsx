6121b512e4bbe6273dcd2daf6626f898b804214d
LouisWackenier <Louis.wackenier@gmail.com>
2024-05-10 06:57:17 +0200

    Fully updated my about page

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -25,16 +25,26 @@ const About = () => {
         <ParagraphTextContainer>
             <SubTitle>Next generation.</SubTitle>
             <SubText>
-             I started coding from a young age and was already amazed by how coding works.
-             And so I strive to become a 10x developer. Besides coding I also do fitness and long distance running.
+            Throughout my journey of learning how to code I have learned how to use different technologies.<br /> 
+            And now I have a good understanding of these powerful last generation technologies<br />
+            Some of these you may know like NextJS, React, etc. These frameworks are an essential part of what a business needs to succeed in the modern sphere of IT.
             </SubText>
         </ParagraphTextContainer>
 
         <ParagraphTextContainer>
             <SubTitle>Crypto</SubTitle>
             <SubText>
-             I started coding from a young age and was already amazed by how coding works.
-             And so I strive to become a 10x developer. Besides coding I also do fitness and long distance running.
+            The decentralized crypto market has been on the rise and lots of people and businesses are using it and yet there are people that are missing out, as for me<br />
+          I have been studying this market ever since it has been becoming mainstream and have been working on projects that have to do with this trend.<br />
+            </SubText>
+        </ParagraphTextContainer>
+
+        <ParagraphTextContainer>
+            <SubTitle>AI</SubTitle>
+            <SubText>
+            From a young age I have had a passion and interest in sci fi and I fell in love with the genre, 
+            And I have been waiting for the chance to learn about AI and now that it has been<br />
+            becoming popular I have been having a keen interest in AI technology and I see it as an essential part in humanity's strive towards the future so I am expecting to be working on some AI projects. 
             </SubText>
         </ParagraphTextContainer>
       </TextContainer>
