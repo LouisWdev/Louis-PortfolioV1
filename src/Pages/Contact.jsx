0c2b90f6583b539f2218488ec882d9ea74ada142
LouisWackenier <Louis.wackenier@gmail.com>
2024-05-14 08:01:33 +0200

    Styled the contact page.

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -4,30 +4,133 @@ import styled from "styled-components";
 const Contact = () => {
   return (
     <>
-      <Navbar/>
-      <FormContainer>
-       <form>
-         <InputContainer><input type="text" name="email" /></InputContainer><br/>
-         <InputContainer><input type="text" name="description" /></InputContainer><br/>
-         <ButtonContainer><button>Send</button></ButtonContainer>
-       </form>
-      </FormContainer>
-    </>
+    <Navbar />
+    <Section id='Contact'>
+      
+    <FormContainer>
+     <Context>
+       If you have any questions or wish to get in contact with me, Please feel free to send a email.
+     </Context>
+     <Form>
+       <FormGroup>
+         <Label htmlFor="name">Name</Label>
+         <NameInput
+           type="text"
+           id="name"
+           name="name"
+           required
+         />
+       </FormGroup>
+       <FormGroup>
+         <Label htmlFor="email">Email</Label>
+         <EmailInput
+           type="email"
+           id="email"
+           name="email"
+           required
+         />
+       </FormGroup>
+       <FormGroup>
+         <MessageLabel htmlFor="message">Message</MessageLabel>
+         <MessageInput
+           id="message"
+           name="message"
+           required
+         />
+       </FormGroup>
+       <Button type="submit">Submit</Button>
+     </Form>
+       </FormContainer>
+    </Section>
+   </>
   )
 }
 
 export default Contact
 
+const Section = styled.div`
+     height: 90vh;
+     display: flex;
+`;
+
 const FormContainer = styled.div`
-    margin-top: 35vh;
-    margin-left: 45vw;
+  margin: 0 auto;
+  margin-top: 200px;
 `;
 
-const InputContainer = styled.div`
-    
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
 `;
 
-const ButtonContainer = styled.div`
-    width: 200px;
-    height: 100px;
+const FormGroup = styled.div`
+  margin-bottom: 5px;
+
+`;
+
+const Label = styled.label`
+  font-size: 20px;
+  margin-bottom: 5px;
+  margin-right: 5px;
+`;
+
+const MessageLabel = styled.label`
+  font-size: 20px;
+  margin-bottom: 5px;
+  margin-right: 5px;
+`;
+
+const NameInput = styled.input`
+  width: 400px;
+  height: 30px;
+  font-size: 20px;
+  margin-bottom: 10px;
+
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+`;
+
+const EmailInput = styled.input`
+   width: 400px;
+  height: 30px;
+  font-size: 20px;
+  margin-bottom: 10px;
+
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+`;
+
+const MessageInput = styled.input`
+  width: 460px;
+  height: 200px;
+  font-size: 20px;
+  margin-bottom: 10px;
+  display: flex;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  
+`;
+
+const Button = styled.button`
+  background-color: #007bff;
+  color: #fff;
+  padding: 10px 20px;
+  font-size: 20px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+
+  transition: background-color 1s;
+
+  &:hover {
+    background-color: lightblue;
+  }
+  
+`;
+
+const Context = styled.h3`
+    width: 25vw;
 `;
