2fc214cbca934d6fced84b853dce40124ea4587f
LouisWackenier <Louis.wackenier@gmail.com>
2024-05-15 07:09:12 +0200

    Home page

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,34 +5,40 @@ import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
-    <Animator>
-      <CenterContainer>
-        <Text as='h1' style={{ color: 'white', textAlign: 'center', marginBottom: '5px'}}>
-          Louis W.
-        </Text>
-        <Text as='h2' style={{ color: 'white', textAlign: 'center', marginBottom: '5px'}}>
-          Welcome to my Portfolio
-        </Text>
-        <Text as='h3' style={{ color: 'white', textAlign: 'center'}}>
-          Feel free to explore my website
-        </Text>
-        <ButtonsContainer>
-          <Link to="Portfolio">
-            <ButtonContainer><Text as='p' style={{ color: 'white', display: 'inline-block', marginRight: '15px' }}>Portfolio</Text></ButtonContainer>
-          </Link>
-          <Link to="Contact">
-           <ButtonContainer><Text as='p' style={{ color: 'white', display: 'inline-block', marginRight: '15px' }}>Contact</Text></ButtonContainer>
-          </Link>
-          <Link to="About">
-            <ButtonContainer><Text as='p' style={{ color: 'white', display: 'inline-block', marginRight: '15px' }}>About</Text></ButtonContainer>
-          </Link>
-        </ButtonsContainer>
-      </CenterContainer>
-    </Animator>
+    <Background>
+       <Animator>
+         <CenterContainer>
+           <Text as='h1' style={{ color: 'white', textAlign: 'center', marginBottom: '5px'}}>
+             Louis W.
+           </Text>
+           <Text as='h2' style={{ color: 'white', textAlign: 'center', marginBottom: '5px'}}>
+             Welcome to my Portfolio
+           </Text>
+           <Text as='h3' style={{ color: 'white', textAlign: 'center'}}>
+             Feel free to explore my website
+           </Text>
+           <ButtonsContainer>
+             <Link to="Portfolio">
+               <ButtonContainer><Text as='p' style={{ color: 'white', display: 'inline-block', marginRight: '15px' }}>Portfolio</Text></ButtonContainer>
+             </Link>
+             <Link to="Contact">
+              <ButtonContainer><Text as='p' style={{ color: 'white', display: 'inline-block', marginRight: '15px' }}>Contact</Text></ButtonContainer>
+             </Link>
+             <Link to="About">
+               <ButtonContainer><Text as='p' style={{ color: 'white', display: 'inline-block', marginRight: '15px' }}>About</Text></ButtonContainer>
+             </Link>
+           </ButtonsContainer>
+         </CenterContainer>
+       </Animator>
+    </Background>
   )
 }
 
 export default Home;
+
+const Background = styled.div`
+
+`;
 
 const CenterContainer = styled.div`
   margin-top: 35vh;
