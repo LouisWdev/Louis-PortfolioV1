de9dd19705f69c2f2983b2c6277f5af9cc1aa50c
LouisWackenier <Louis.wackenier@gmail.com>
2024-06-10 06:53:04 +0200

    Removed use effect

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,72 +1,39 @@
-import { Animator } from '@arwes/react-animator';
-import { Text } from '@arwes/react-text';
+//REACT
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
+// ARWES 
+import { Animator } from '@arwes/react-animator';
+import { Text } from '@arwes/react-text';
+
+
+
 const Home = () => {
+  const [active] = useState(true);
+
   return (
-    <Background>
-       <Animator>
-         <CenterContainer>
-           <Text as='h1' style={{ color: 'white', textAlign: 'center', marginBottom: '5px'}}>
+       <Animator active={active} duration={{ enter: 2.5, exit: 1.5 }}>
+           <Text as='h1' style={{textAlign: 'center', marginBottom: '5px'}}>
              Louis W.
            </Text>
-           <Text as='h2' style={{ color: 'white', textAlign: 'center', marginBottom: '5px'}}>
+           <Text as='h2' style={{textAlign: 'center', marginBottom: '5px'}}>
              Welcome to my Portfolio
            </Text>
-           <Text as='h3' style={{ color: 'white', textAlign: 'center'}}>
+           <Text as='h3' style={{textAlign: 'center'}}>
              Feel free to explore my website
            </Text>
-           <ButtonsContainer>
              <Link to="Portfolio">
-               <ButtonContainer><Text as='p' style={{ color: 'white', display: 'inline-block', marginRight: '15px' }}>Portfolio</Text></ButtonContainer>
+               <Text as='p' style={{ color: 'white', display: 'inline-block', marginRight: '15px' }}>Portfolio</Text>
              </Link>
              <Link to="Contact">
-              <ButtonContainer><Text as='p' style={{ color: 'white', display: 'inline-block', marginRight: '15px' }}>Contact</Text></ButtonContainer>
+              <Text as='p' style={{ color: 'white', display: 'inline-block', marginRight: '15px' }}>Contact</Text>
              </Link>
              <Link to="About">
-               <ButtonContainer><Text as='p' style={{ color: 'white', display: 'inline-block', marginRight: '15px' }}>About</Text></ButtonContainer>
+               <Text as='p' style={{ color: 'white', display: 'inline-block', marginRight: '15px' }}>About</Text>
              </Link>
-           </ButtonsContainer>
-         </CenterContainer>
        </Animator>
-    </Background>
   )
 }
 
 export default Home;
-
-const Background = styled.div`
-
-`;
-
-const CenterContainer = styled.div`
-  margin-top: 35vh;
-  font-size: 2.3vh;
-`;
-
-const ButtonsContainer = styled.div`
-  margin-top: 5px;
-  text-align: center;
-  cursor: pointer;
-`;
-
-const ButtonContainer = styled.div`
-    width: 75px;
-    font-weight: 350;
-    font-size: 2vh;
-    padding: 8px 10px 8px 12px;
-    margin-top: 10px;
-    margin-right: 15px;
-    text-align: center;
-    display: inline-block;
-    transition: border 0.5s ease-in;
-    border: 2px solid white;
-    border-radius: 5px;
-     
-    &:hover {
-      border: 2px solid red;
-      border-radius: 5px;
-      color: red;
-    }
-`;
