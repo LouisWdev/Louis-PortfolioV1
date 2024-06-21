99ce0117f065e91dfe7c4d9c5de239e5d6316867
LouisWackenier <Louis.wackenier@gmail.com>
2024-06-21 08:52:16 +0200

    Font size

--- src/Home.jsx
+++ src/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
 
   return (
      <MainTextContainer>
-           <TextContainer>
+           <TextContainer style={{fontSize: '2vh'}}>
              Louis W.
            </TextContainer>
            <TextContainer>
